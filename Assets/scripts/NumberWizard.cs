5fabcf90ae5819a61dd5f6f1a5853f38f22c663d
James <j.e.gotsell@gmail.com>
2018-04-05 20:50:50 +0100

    removed comments

--- Assets/scripts/NumberWizard.cs
+++ Assets/scripts/NumberWizard.cs
@@ -2,54 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NumberWizard : MonoBehaviour {
-	int max;
-	int min;
-	int guess;
+public class NumberWizard : MonoBehaviour
+{
+  int max;
+  int min;
+  int guess;
 
-	// Use this for initialization
-	void Start () {
-		StartGame();
-	}
 
-	void StartGame () {
-		max = 1000;
-		min = 1;
-		guess = 500;
-		max = max + 1;
-		print("=================================");
-		print("welcome to number wizard");
-		print("pick a number but don't tell me");
+  void Start()
+  {
+    StartGame();
+  }
 
-		print("the highest number you can pick is " + max);
-		print("the lowest you can pick is " + min);
+  void StartGame()
+  {
+    max = 1000;
+    min = 1;
+    guess = 500;
+    max = max + 1;
+    print("=================================");
+    print("welcome to number wizard");
+    print("pick a number but don't tell me");
 
-		print("is the number higher or lower than" + guess);
+    print("the highest number you can pick is " + max);
+    print("the lowest you can pick is " + min);
 
-		print("up arrow for higher, down arrow for lower , return for equal");
-		max = max + 1;
-	}
+    print("is the number higher or lower than" + guess);
 
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetKeyDown(KeyCode.UpArrow)) {
-			//print("up arrow press");
-			min = guess; 
-			nextGuess();
-		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			//print("down arrow press");
-			max = guess; 
-			nextGuess();
+    print("up arrow for higher, down arrow for lower , return for equal");
+    max = max + 1;
+  }
 
-		} else if (Input.GetKeyDown(KeyCode.Return)) {
-			print("i won");
-			StartGame();
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.UpArrow))
+    {
+      min = guess;
+      nextGuess();
+    }
+    else if (Input.GetKeyDown(KeyCode.DownArrow))
+    {
 
-		};
-	}
-	void nextGuess (){
-			guess = (max + min) / 2;
-			print("higher or lower than " + guess);
-			print("up arrow for higher, down arrow for lower , return for equal");
-	}
+      max = guess;
+      nextGuess();
+
+    }
+    else if (Input.GetKeyDown(KeyCode.Return))
+    {
+      print("i won");
+      StartGame();
+
+    };
+  }
+  void nextGuess()
+  {
+    guess = (max + min) / 2;
+    print("higher or lower than " + guess);
+    print("up arrow for higher, down arrow for lower , return for equal");
+  }
 }
